36e7afcdbf3e84de04edcf75d1f14ea9219dc343
Peter Decuyper <github@serial-graphics.be>
2015-03-11 12:11:04 +0100

    Added support for local aliases folder. + bash_profile.sh loads only scripts with .sh extension.

--- bash_profile.sh
+++ bash_profile.sh
@@ -7,7 +7,17 @@ BASH_PROFILES=`dirname ~/.bash_profiles/bash_profiles.sh`
 # Load all scripts in the .bash_profiles/enabled/ subdirectory (if any).
 if [ "$(ls $BASH_PROFILES/enabled)" ]; then
 
-  for file in $BASH_PROFILES/enabled/*;
+  for file in $BASH_PROFILES/enabled/*.sh;
+  do
+    source $file
+  done
+
+fi
+
+# Load all aliases in the .bash_profiles/aliases/ subdirectory (if any).
+if [ "$(ls $BASH_PROFILES/aliases)" ]; then
+
+  for file in $BASH_PROFILES/aliases/*.sh;
   do
     source $file
   done
