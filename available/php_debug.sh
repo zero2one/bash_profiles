f78aa9612f4e87a7270df011b21cb61edc2f8890
Peter Decuyper <github@serial-graphics.be>
2015-07-02 11:15:06 +0200

    Switched to Xdebug for debugging PHP scripts

--- available/php_debug.sh
+++ available/php_debug.sh
@@ -11,5 +11,5 @@
 #    $ php-debug-off
 ##
 
-alias php-debug-on='export QUERY_STRING="start_debug=1&debug_stop=1&debug_fastfile=1&debug_coverage=1&use_remote=1&send_sess_end=1&debug_session_id=2000&debug_start_session=1&debug_new_session=1&debug_port=10137&debug_host=localhost"'
-alias php-debug-off='export QUERY_STRING=""'
+alias phpdebug-on="export XDEBUG_CONFIG='idekey=PHPSTORM'"
+alias phpdebug-off="unset XDEBUG_CONFIG"
