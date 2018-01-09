1fab90288a2f49f785578aee55051a8430054175
Peter Decuyper <zero2one@serial-graphics.com>
2018-01-09 15:24:58 +0100

    Added the user bin directory to the paths.

--- available/composer.sh
+++ available/composer.sh
@@ -36,3 +36,4 @@ alias composer="php_no_xdebug /usr/local/bin/composer"
 # On MacOS with composer installed using brew, comment previous line
 # Install jq by executing `brew install jq` and uncomment following line.
 # alias composer="php_no_xdebug /usr/local/Cellar/composer/`brew info --json=v1 composer | jq -r '.[0].installed[0].version'`/libexec/composer.phar"
+
