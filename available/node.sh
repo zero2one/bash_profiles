9cc7437a8afe3ab27d47490b1c9d515971e1e3c0
Peter Decuyper <zero2one@serial-graphics.com>
2018-01-09 15:26:47 +0100

    Added support for the Node Version Manager.

---
+++ available/node.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+##
+# Support for Node Version Manager.
+##
+
+# The node Version Manager can be installed using Brew but there are some caveats.
+# Run $ brew info nvm after installation to see what should be added.
+
+export NVM_DIR="$HOME/.nvm"
+. "/usr/local/opt/nvm/nvm.sh"
+
